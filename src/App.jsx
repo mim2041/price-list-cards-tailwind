105f7ae54cf3316af605310ff7ff3b26bbde2edc
mim2041 <suraiyamim338@gmail.com>
2023-04-04 11:33:33 +0600

    a simple navber responsive menu bar made

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1 className='text-5xl text-purple-600'>Hello from Tailwind</h1>
+      
+    </div>
+  )
+}
+
+export default App
